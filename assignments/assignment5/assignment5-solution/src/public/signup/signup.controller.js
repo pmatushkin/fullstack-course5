8a6c88556144bc45dcd710237f312a518d5b4a05
Pavel Matushkin <pmatushkin@gmail.com>
2018-03-07 10:21:32 -0800

    fix: Fix form validation

--- assignments/assignment5/assignment5-solution/src/public/signup/signup.controller.js
+++ assignments/assignment5/assignment5-solution/src/public/signup/signup.controller.js
@@ -17,17 +17,19 @@
             item: {}
         };
 
+        // For details on setting validity of an input field see https://stackoverflow.com/a/22986254
         ctrl.getMenuItem = function () {
+            // To invalidate the form (including the Submit button) set field validity to FALSE until the validation completes.
+            $scope.signupForm.itemShortName.$setValidity("itemShortName", false);
+
             var promise = MenuService.getMenuItemByShortName(ctrl.user.itemShortName);
             promise.then(function (response) {
-                // Set validity of an input field.
-                // For details see https://stackoverflow.com/a/22986254
+                // Validation completed, the field content is a valid menu item short name. Set validity to TRUE.
                 $scope.signupForm.itemShortName.$setValidity("itemShortName", true);
                 ctrl.user.item = response.data;
             })
                 .catch(function (error) {
-                    // Set validity of an input field.
-                    // For details see https://stackoverflow.com/a/22986254
+                    // Validation completed, the field content is NOT a valid menu item short name. Set validity to FALSE.
                     $scope.signupForm.itemShortName.$setValidity("itemShortName", false);
                     ctrl.user.item = {};
                 });
