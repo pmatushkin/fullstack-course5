d10b61eecca8d8756a65edf8d7fbf19244b9af5c
Pavel Matushkin <pmatushkin@gmail.com>
2018-03-07 14:45:58 -0800

    fix: Fix API endpoint

--- assignments/assignment5/assignment5-solution/src/common/common.module.js
+++ assignments/assignment5/assignment5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'http://pmatushkin-angularcourse5.herokuapp.com')
+.constant('ApiPath', 'https://pmatushkin-angularcourse5.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
