610b98bfdc0fd29d74e7551bbdb2990914391381
Pavel Matushkin <pmatushkin@gmail.com>
2018-03-06 16:33:15 -0800

    feat: Implement form validation

--- assignments/assignment5/assignment5-solution/src/public/signup/signup.controller.js
+++ assignments/assignment5/assignment5-solution/src/public/signup/signup.controller.js
@@ -4,8 +4,38 @@
     angular.module('public')
         .controller('SignupController', SignupController);
 
-    // SignupController.$inject = ['menuCategories'];
-    function SignupController() {
+    SignupController.$inject = ['$scope', 'MenuService'];
+    function SignupController($scope, MenuService) {
+        var ctrl = this;
+
+        ctrl.user = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phoneNumber: '',
+            itemShortName: '',
+            item: {}
+        };
+
+        ctrl.getMenuItem = function () {
+            var promise = MenuService.getMenuItemByShortName(ctrl.user.itemShortName);
+            promise.then(function (response) {
+                // Set validity of an input field.
+                // For details see https://stackoverflow.com/a/22986254
+                $scope.signupForm.itemShortName.$setValidity("itemShortName", true);
+                ctrl.user.item = response.data;
+            })
+                .catch(function (error) {
+                    // Set validity of an input field.
+                    // For details see https://stackoverflow.com/a/22986254
+                    $scope.signupForm.itemShortName.$setValidity("itemShortName", false);
+                    ctrl.user.item = {};
+                });
+        }
+
+        ctrl.submit = function () {
+            console.log('user', ctrl.user);
+        }
     }
 
 })();
